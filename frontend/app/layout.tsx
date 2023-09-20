cd3cc6363d4a0c04a9df0ac12ee82fbbf0e48a10
king-krs-one <jonathan.nickels@googlemail.com>
2023-09-20 09:38:10 +0200

    setup redux toolkit

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,27 +1,30 @@
-import '@/styles/globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Navbar, Footer } from '@/components/layout'
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Provider from "@/redux/provider";
+import { Navbar, Footer } from "@/components/layout";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'School of Greatness',
-  description: 'Online platform to become a person of greatness',
-}
+  title: "School of Greatness",
+  description: "Online platform to become a person of greatness",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
+        <Provider>
+          <Navbar />
+          {children}
+          <Footer />
+        </Provider>
       </body>
     </html>
-  )
+  );
 }
---
+++ frontend/redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const makeStore = () => {
+    return configureStore({
+        reducer: {},
+        devTools:process.env.NODE_ENV !== 'production'
+    })
+}
+
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
