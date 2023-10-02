97b41a65d8991b9f774a1b13fe6aee63bbff253f
king-krs-one <jonathan.nickels@googlemail.com>
2023-10-03 00:10:41 +0200

    Add Dashboard page content and List component

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,8 +1,42 @@
+"use client"
+
+import { Heading, List, Spinner } from "@/components/widgets";
+import { useRetrieveUserQuery } from "@/redux/features/authApiSlice";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+  const { data: user, isLoading, isError } = useRetrieveUserQuery();
+  const config = [
+    { label: "First Name", value: user?.first_name },
+    { label: "Last Name", value: user?.last_name },
+    { label: "Email", value: user?.email },
+  ];
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center my-8">
+        <Spinner lg />
+      </div>
+    );
+  }
+
+  if (isError) {
+    router.push("/auth/login");
+    toast.error("Please log in");
+  }
+
   return (
-    <main>
-      <h1>Dashboard</h1>
-    </main>
-  )
+    <>
+      <header className="mx-auto max-w-7xl bg-white shadow">
+        <div className="px-4 py-6 sm:px-6 lg:px-8">
+          <Heading level="h1">Dashboard</Heading>
+        </div>
+      </header>
+      <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <List config={config} />
+      </main>
+    </>
+  );
 }
---
+++ frontend/components/widgets/List.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Spinner } from ".";
+
+interface ConfigProps {
+  label: string;
+  value: string | undefined;
+}
+
+interface ListProps {
+  config: ConfigProps[];
+}
+
+// Usage example:
+const List: React.FC<ListProps> = ({ config }) => {
+  return (
+    <ul role="list" className="divide-y divide-gray-100">
+      {config.map(({ label, value }) => {
+        return (
+          <li className="flex justify-between gap-x-6 py-5">
+            <div>
+              <p className="text-sm font-semibold leading-6 text-gray-900">
+                {label}
+              </p>
+            </div>
+            <div>
+              <p className="text-sm font-semibold leading-6 text-gray-900">
+                {value || <Spinner sm />}
+              </p>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default List;
