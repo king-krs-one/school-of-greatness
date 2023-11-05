4a1a38748c6c04178cb0c281549d65b2262a449d
Jonathan Nickels <jonathan.nickels@engrotec.de>
2023-11-05 13:03:50 +0100

    renamed backend folder school_of_greatness to app

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for school_of_greatness project.
+URL configuration for open_media project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- backend/app/wsgi.py
+++ backend/app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for school_of_greatness project.
+WSGI config for open_media project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_of_greatness.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
